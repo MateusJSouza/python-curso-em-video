d4dfdfde244282bc11bd185d72a2679e6a8f621f
MateusJSouza <mateusjdev@gmail.com>
2024-09-27 15:40:36 -0300

    exercício 007

--- python-exercises/ex007.py
+++ python-exercises/ex007.py
@@ -1,16 +1,6 @@
-measure = float(input('Digite uma distância em metros: '))
+grade1 = float(input('Digite a primeira nota do aluno: '))
+grade2 = float(input('Digite a segunda nota do aluno: '))
 
-# Conversões
-km = measure / 1000
-hm = measure / 100
-dam = measure / 10
-dm = measure * 10
-cm = measure * 100
-mm = measure * 1000
+average = (grade1 + grade2) / 2
 
-print(f'A média de {measure}m corresponde a {cm}cm e {mm}mm')
-print(f'Hectômetros: {hm} hm')
-print(f'Decâmetros: {dam} dam')
-print(f'Decímetros: {dm} dm')
-print(f'Centímetros: {cm} cm')
-print(f'Milímetros: {mm} mm')
+print(f'A média entre {grade1:.1f} e {grade2:.1f} é igual a {average:.1f}')
