31fb6561435232c016c1b17bf8ab8515e60dc959
MateusJSouza <mateusjdev@gmail.com>
2024-09-27 15:23:05 -0300

    exercício 006

---
+++ python-exercises/ex006.py
@@ -0,0 +1,6 @@
+grade1 = float(input('Digite a primeira nota do aluno: '))
+grade2 = float(input('Digite a segunda nota do aluno: '))
+
+average = (grade1 + grade2) / 2
+
+print(f'A média entre {grade1:.1f} e {grade2:.1f} é igual a {average:.1f}')
