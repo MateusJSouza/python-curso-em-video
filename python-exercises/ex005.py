f4449c98bb6a25020614a669bf73be6e7991eb42
MateusJSouza <mateusjdev@gmail.com>
2024-09-27 15:16:17 -0300

    exercício 005

---
+++ python-exercises/ex005.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número: '))
+
+predecessor = n - 1 # antecessor
+successor = n + 1 # sucessor
+
+print(f'Analisando o valor {n}, seu antecessor é {predecessor} e o sucessor é {successor}')
