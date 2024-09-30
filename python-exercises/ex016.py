eb9b943d14389da54c390098e5df004989fefda2
MateusJSouza <mateusjdev@gmail.com>
2024-09-30 14:30:53 -0300

    exercício 16

--- python-exercises/ex016.py
+++ python-exercises/ex016.py
@@ -1,5 +1,5 @@
-import math
+from math import trunc
 
 num = float(input('Digite um valor: '))
 
-print('O valor digitado foi {} e sua porção inteira é {}'.format(num, math.trunc(num)))
+print('O valor digitado foi {} e sua porção inteira é {}'.format(num, trunc(num)))
