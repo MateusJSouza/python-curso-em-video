b2438f8759b18197c5ac8e753b2615be06b8179f
MateusJSouza <mateusjdev@gmail.com>
2024-09-30 14:47:11 -0300

    refactor no exercício 18

--- python-exercises/ex18.py
+++ python-exercises/ex18.py
@@ -1,14 +1,14 @@
-import math
+from math import sin, cos, tan, radians
 
 angle = float(input('Digite o ângulo que você deseja: '))
 
 # Convertendo graus para radianos
-radians = math.radians(angle)
+radians = radians(angle)
 
 # Cálculo do seno, cosseno e tangente
-sine = math.sin(radians)
-cosine = math.cos(radians)
-tangent = math.tan(radians)
+sine = sin(radians)
+cosine = cos(radians)
+tangent = tan(radians)
 
 print(f'O ângulo de {angle}° tem o SENO de: {sine:.2f}')
 print(f'O ângulo de {angle}° tem o COSSENO de: {cosine:.2f}')
